78556cd1fe4c972c54f5a09eacee7675782897a6
bdickason <dickason@gmail.com>
2015-03-08 16:09:42 -0700

    User can now login successfully with steamguard

--- cfg/config.js
+++ cfg/config.js
@@ -1,3 +1,4 @@
 // Steam account info
 exports.STEAM_LOGIN = process.env.STEAM_LOGIN || '';
 exports.STEAM_PASSWORD = process.env.STEAM_PASSWORD || '';
+exports.STEAM_SENTRY = process.env.STEAM_SENTRY || '';
--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 var Steam = require('steam');
+var fs = require('fs');
 var cfg = require('./cfg/config.js');
 
-console.log(cfg.STEAM_LOGIN);
-console.log(cfg.STEAM_PASSWORD);
+// Setup login and password
+var credentials = {
+	accountName: cfg.STEAM_LOGIN,
+	password: cfg.STEAM_PASSWORD
+};
+
+var sentryPath = '.shaSentry';
+
+// Setup steam Sentry buffer (passcode for account)
+if(fs.existsSync(sentryPath)) {
+	credentials['shaSentryfile'] = fs.readFileSync(sentryPath);
+}
+
+var bot = new Steam.SteamClient();
+
+// Log in with credentials specified in config file
+bot.logOn(credentials);
+
+bot.on('loggedOn', function() {
+	console.log('Logged in!');
+});
+
+// If user has SteamGuard - Used to get a 'shaSentryfile' which allows us to login without entering a access code every time
+bot.on('sentry', function(sha) {
+	// We don't have a sentry sha, let's store it.
+	var path = sentryPath;
+
+	fs.open(path, 'w', function(err, fd) {
+		if(err) {
+			throw 'error opening file: ' + err;
+		}
+		else {
+			fs.write(fd, sha, 0, sha.length, null, function(err) {
+				if (err) {
+					throw 'error writing file: ' + err;
+				}
+				else {
+					fs.close(fd, function() {
+						console.log('sha saved, you can now login');
+					});
+				}
+			});
+		}
+	});
+});
